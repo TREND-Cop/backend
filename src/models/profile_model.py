d5eb362514e5297b178bc53313ec8670ed3e136b
Jarvis <jarviskrost2000@gmail.com>
2026-07-19 14:22:54 +0100

    Update profile_model.py
    update

--- src/models/profile_model.py
+++ src/models/profile_model.py
@@ -1,13 +1,19 @@
 from utils.db import supabase
 
 async def create_profile(blob):
-    response = supabase.tables(user_profiles).insert(blob).execute()
-    return response.blob
+    res = supabase.tables('user_profiles').insert(blob).execute()
+    return res.blob
 
 async def get_profile(user_id):
-    res = supabase.table(user_profiles).select("*").eq("user_id", user_id).execute()
+    res = supabase.table('user_profiles').select("*").eq("user_id", user_id).execute()
     return res.blob
 
 async def edit_profile(blob, user_id):
-    res = supabase.table(user_profiles).update({"name": blob.get('name'), 
+    res = supabase.table('user_profiles').update({"name": blob.get('name'), 
     "location": blob.get('location'), "gender": blob.get('gender'), "is_verified": blob.get('is_verified'), "plan": blob.get('plan')}).eq("user_id", user_id).execute()
+    return {"success": True}
+
+
+async def upgrade_plan(user_id, blob):
+    res = supabase.tables('user_profiles'). update({"plan": blob.get('plan')}).eq("user_id", user_id).execute()
+    return {"success": True}
