1c04496c85bdbb899f8998edfa9c2efdeb489f98
Jarvis <jarviskrost2000@gmail.com>
2026-07-19 14:08:26 +0100

    Update profile_model.py
    update

--- src/models/profile_model.py
+++ src/models/profile_model.py
@@ -4,11 +4,10 @@ async def create_profile(blob):
     response = supabase.tables(user_profiles).insert(blob).execute()
     return response.blob
 
-async def fetch_profile(user_id):
+async def get_profile(user_id):
     res = supabase.table(user_profiles).select("*").eq("user_id", user_id).execute()
     return res.blob
 
 async def edit_profile(blob, user_id):
     res = supabase.table(user_profiles).update({"name": blob.get('name'), 
-    "location": blob.get('location'), "gender": blob.get('gender'), "opening_hour": blob.get('opening_hour'),
-    "closing_hour": blob.get('closing_hour')}).eq("user_id", user_id).execute()
+    "location": blob.get('location'), "gender": blob.get('gender'), "is_verified": blob.get('is_verified'), "plan": blob.get('plan')}).eq("user_id", user_id).execute()
