b75f9bf4686192520c8838f6d6bba111ac23bfea
Jarvis <jarviskrost2000@gmail.com>
2026-09-03 21:24:36 +0100

    Update metadata_model.py

--- src/models/metadata_model.py
+++ src/models/metadata_model.py
@@ -20,7 +20,7 @@ async def get_business_image_url(business_id, limit):
     return {"image_url": [res], "success": True}
 
 
-async def edit_business(id, user_id, new_url):
+async def edit_image(id, user_id, new_url):
     # he passes in the old_url in the controller
     res = supabase.table('user_metadata).update({"url": new_url}).eq("id", id).eq("user_id", user_id).select().execute()
     return {"updated_image": new_url, "success": True}
