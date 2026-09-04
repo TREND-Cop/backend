f1d0ac81606ced82e6488374b1f965788b79faaf
Jarvis <jarviskrost2000@gmail.com>
2026-09-04 16:03:14 +0100

    Update review_model.py

--- src/models/review_model.py
+++ src/models/review_model.py
@@ -12,10 +12,18 @@ async def delete_review(user_id, business_id, id):
 
 
 
-async def get_review(business_id):
-    res = supabase.table('business_review').select("*").eq("business_id", business_id).execute()
-    return {"reviews": res, "success": True}
-    return = res.blob
+async def get_reviews(business_id, limit, offset):
+    try:
+      res = supabase.table('business_review')/
+.select("*")/
+.eq("business_id", business_id)/
+.order("created_at", desc=False)/
+.range(offset, (offset + limit -1))/
+.execute()
+      return {"reviews": res, "success": True}
+    except Exception as e:
+      return {"message": "Error fetching reviews"}
+    
 
 
 
